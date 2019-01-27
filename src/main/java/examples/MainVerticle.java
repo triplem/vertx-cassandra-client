fe8448b2822aa3862a20027ec34536116bcf814b
triplem <160079+triplem@users.noreply.github.com>
2019-01-27 21:49:27 +0100

    Add reproducer

---
+++ src/main/java/examples/MainVerticle.java
@@ -0,0 +1,27 @@
+package examples;
+
+import io.vertx.core.AbstractVerticle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class MainVerticle extends AbstractVerticle {
+
+  private static final Logger logger = LoggerFactory.getLogger(MainVerticle.class);
+
+  public void start() {
+    logger.debug("Start ServiceVerticle");
+
+    vertx.deployVerticle("examples.ServiceVerticle", ar -> {
+      if (ar.failed()) {
+        logger.error("Could not start serviceVerticle", ar.cause());
+      } else {
+        logger.error("........................................");
+        logger.error("ar: ", ar.result());
+      }
+    });
+
+    logger.info("All verticles deployed");
+
+  }
+}
---
+++ src/main/java/examples/ServiceVerticle.java
@@ -0,0 +1,40 @@
+package examples;
+
+import io.vertx.cassandra.CassandraClient;
+import io.vertx.cassandra.CassandraClientOptions;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ServiceVerticle extends AbstractVerticle {
+
+  private static final Logger logger = LoggerFactory.getLogger(ServiceVerticle.class);
+
+  private static final String CASSANDRA_KEYSPACE = "dummy";
+
+  public void start(Future<Void> startFuture) {
+    CassandraClientOptions options = new CassandraClientOptions();
+    CassandraClient client = CassandraClient.createShared(vertx, options);
+
+    client.connect(CASSANDRA_KEYSPACE, ar -> {
+      if (ar.failed()) {
+        logger.error("ALL IS BAD....");
+        if (client.isConnected()) {
+          client.disconnect();
+        }
+        // does not work
+        //startFuture.fail(ar.cause());
+
+        // does work
+        startFuture.complete();
+      } else {
+        logger.error("THIS IS NEVER REACHED ON ERROR");
+
+        startFuture.complete();
+      }
+    });
+
+    logger.info("Connecting cassandra");
+  }
+}
